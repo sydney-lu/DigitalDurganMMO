b72c7160b0f7c3521b5fb3c9d2d3ee9fb4d2f78e
PauloAugusto <pauloaugustogp@gmail.com>
2018-02-09 20:13:35 -0500

    Changes on Enemy Ai, Adding Behavior Tree, Blackbord, Nodes for the Tree and Blueprint for enemy

--- DDMMO/Source/DDMMO/AI/AIBaseAgent.h
+++ DDMMO/Source/DDMMO/AI/AIBaseAgent.h
@@ -12,21 +12,49 @@ class DDMMO_API AAIBaseAgent : public ACharacter
 	GENERATED_BODY()
 
 	private:
+		UPROPERTY(EditDefaultsOnly)
+			class UCharacterClass* target;
 
 	protected:
-		// Called when the game starts or when spawned
+		UPROPERTY(EditDefaultsOnly)
+			float maxHealth;
+		float currentHealth;
+
+		UPROPERTY(EditDefaultsOnly)
+			float damage;
+
+		UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+			float attackDistance;
+
 		virtual void BeginPlay() override;
 
 	public:
 		UPROPERTY(EditAnywhere, Category = "AI")
 			class UBehaviorTree* BehaviorTree;
 
+		UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+			bool bIsPatroling;
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+			bool bIsReturning;
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+			bool bIsChasing;
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+			bool bIsAttacking;
+
+
 		AAIBaseAgent();
 
-		// Called every frame
 		//virtual void Tick(float DeltaTime) override;
 
-		// Called to bind functionality to input
 		virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+		FORCEINLINE bool IsAlive() const { return currentHealth > 0.0f; }
+		FORCEINLINE float GetHeathRatio() const { return (currentHealth/maxHealth); }
+		FORCEINLINE float GetCurrentHealth() const { return currentHealth; }
+		FORCEINLINE float GetDamage() const { return damage; }
+		FORCEINLINE UCharacterClass* GetAgentTarget() const { return target; }
 
 };
